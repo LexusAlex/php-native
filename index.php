f51be288be3c878795c7b3a3de042cb8882a4b7c
alex <alexsey_89@bk.ru>
2018-12-16 23:14:40 +0300

    add mysql and postgres db

--- index.php
+++ index.php
@@ -4,10 +4,27 @@ declare(strict_types=1);
 
 //phpinfo();
 
-$pdo = new PDO('mysql:dbname=root;host=db;port=3306','root', 'root',[
+// проверка работы баз данных
+$mariadb = new PDO('mysql:dbname=root;host=mariadb;port=3306','root', 'root',[
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
 ]);
 
-var_dump($pdo->query('SELECT * FROM test')->fetchAll());
+var_dump($mariadb->query('SELECT VERSION()')->fetchAll());
+
+$mysql = new PDO('mysql:dbname=root;host=mysql;port=3306','root', 'root',[
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+]);
+
+var_dump($mysql->query('SELECT VERSION()')->fetchAll());
+
+$postgres = new PDO('pgsql:host=127.0.0.1;port=5432;dbname=root','root', 'root',[
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+]);
+
+var_dump($postgres->query('SELECT VERSION()')->fetchAll());
