c1202b8ab2d220845bc23986e96ca67c0feb7f7c
alex <alexsey_89@bk.ru>
2018-12-17 22:58:45 +0300

    add new servers

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $mysql = new PDO('mysql:dbname=root;host=mysql;port=3306','root', 'root',[
 
 var_dump($mysql->query('SELECT VERSION()')->fetchAll());
 
-$postgres = new PDO('pgsql:host=127.0.0.1;port=5432;dbname=root','root', 'root',[
+$postgres = new PDO('pgsql:host=postgres;port=5432;dbname=root','root', 'root',[
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
